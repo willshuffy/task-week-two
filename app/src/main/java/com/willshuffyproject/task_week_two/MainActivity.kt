cc43fe6ca894fcd2dc2203d09ef5c46c0636688b
willshuffy <saputrawilly26@gmail.com>
2020-11-17 06:50:28 +0700

    Starter Project

---
+++ app/src/main/java/com/willshuffyproject/task_week_two/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.willshuffyproject.task_week_two
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
